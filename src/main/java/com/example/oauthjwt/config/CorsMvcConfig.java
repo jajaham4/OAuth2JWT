f5ea373661b95c9dc8178f69295b4ca4db07a4d4
jajaham4 <jajaham4@naver.com>
2024-08-27 18:26:31 +0900

    first upload

---
+++ src/main/java/com/example/oauthjwt/config/CorsMvcConfig.java
@@ -0,0 +1,19 @@
+package com.example.oauthjwt.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// MVC 컨트롤 허용모드
+
+@Configuration
+public class CorsMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+
+        corsRegistry.addMapping("/**")
+                .exposedHeaders("Set-Cookie")
+                .allowedOrigins("http://localhost:3000");
+    }
+}
